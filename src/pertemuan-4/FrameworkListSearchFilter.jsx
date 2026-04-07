8a31c1abb5efca837bbfa63e4f43399947c52169
alyah <alyah24si@mahasiswa.pcr.ac.id>
2026-04-07 15:28:19 +0700

    penambahan search dan filter

--- src/pertemuan-4/FrameworkListSearchFilter.jsx
+++ src/pertemuan-4/FrameworkListSearchFilter.jsx
@@ -1,31 +1,41 @@
+import { useState } from "react";
 import frameworkData from "./framework.json";
 
-export default function FrameworkList() {
+export default function FrameworkListSearchFilter() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedTag, setSelectedTag] = useState("");
+
+    /** Deklrasai Logic Search & Filter **/
+    const _searchTerm = searchTerm.toLowerCase();
+    const filteredFrameworks = frameworkData.filter((framework) => {
+        const matchesSearch =
+            framework.name
+                .toLowerCase()
+                .includes(_searchTerm) ||
+            framework.details.developer
+                .toLowerCase()
+                .includes(_searchTerm) ||
+            framework.details.releaseYear
+                .toString()
+                .includes(_searchTerm) ||
+            framework.description
+                .toLowerCase()
+                .includes(_searchTerm);
+                
+
+        const matchesTag = selectedTag ? framework.tags.includes(selectedTag) : true;
+
+        return matchesSearch && matchesTag;
+    });
+
+
+    /** Deklarasi pengambilan unique tags di frameworkData **/
+    const allTags = [
+        ...new Set(frameworkData.flatMap((framework) => framework.tags)),
+    ];
+
     return (
-        // <div className="p-8">
-        //     {frameworkData.map((item) => (
-        //         <div key={item.id} className="border p-4 mb-4 rounded-lg shadow-md bg-white">
-        //             <h2 className="text-lg font-bold text-gray-800">{item.name}</h2>
-        //             <p className="text-gray-600">{item.description}</p>
 
-        //             <p className="text-sky-900 bg-gray-300 w-fit h-5 flex items-center 
-        //                 p-2 rounded-2xl">
-        //                 {item.details.developer}
-        //                 <span className="font-bold">({item.details.releaseYear})</span>
-        //             </p>
-
-        //             <a href={item.details.officialWebsite}
-        //                 className="text-blue-600 underline" target="_blank">Visit Website</a>
-
-        //             {item.tags.map((tag, index) => (
-        //                 <span key={index} className="bg-pink-200 text-gray-700 px-2 py-1 text-xs rounded-full mr-2">
-        //                     {tag}
-        //                 </span>
-        //             ))}
-
-        //         </div>
-        //     ))}
-        // </div>
         <div className="p-8 bg-sky-50 min-h-screen"> {/* Background luar biru muda awan */}
             <div className="max-w-4xl mx-auto grid gap-6">
 
@@ -37,7 +47,29 @@ export default function FrameworkList() {
                     <p className="text-sky-700">Ayo pelajari framework seru bersama Pororo dan teman-teman!</p>
                 </div>
 
-                {frameworkData.map((item) => (
+                <input
+                    type="text"
+                    name="searchTerm"
+                    placeholder="Search framework..."
+                    className="w-full p-2 border border-gray-300 rounded mb-4"
+                    onChange={(e)=>setSearchTerm(e.target.value)}
+                />
+
+                <select
+                    name="selectedTag"
+                    className="w-full p-2 border border-gray-300 rounded mb-4"
+                     onChange={(e)=>setSelectedTag(e.target.value)}
+                >
+                    <option value="">All Tags</option>
+                    {allTags.map((tag, index) => (
+                        <option key={index} value={tag}>
+                            {tag}
+                        </option>
+                    ))}
+                </select>
+
+
+                {filteredFrameworks.map((item) => (
                     <div key={item.id} className="border-4 border-sky-100 p-6 rounded-3xl shadow-lg bg-white relative overflow-hidden group hover:border-orange-200 transition-all duration-300">
 
                         {/* Dekorasi Sudut (Aksen Oranye ala paruh Pororo) */}
