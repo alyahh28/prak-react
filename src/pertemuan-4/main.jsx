88a94472f54c8057e06de9c289ebbbf53fe9b38c
alyahh <alyah24si@mahasiswa.pcr.ac.id>
2026-04-14 22:05:58 +0700

    P5 fix

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,16 +1,12 @@
 import { createRoot } from "react-dom/client";
 import TailwindCSS from "./TailwindCSS";
 import "./tailwind.css"
-import FrameworkList from "./FrameworkList";
-import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
-import ResponsiveDesign from "./ResponsiveDesign";
+import Dashboard from "../pertemuan-5/pages/Dashboard";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div >
-      {/* <FrameworkList/> */}
-      <ResponsiveDesign/>
-      {/* <FrameworkListSearchFilter/> */}
+      <Dashboard/>
         </div>
     )
